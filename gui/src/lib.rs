8c90dd552ec2ddc882466e8c09fb67ea25022276
Sarah Azouvi <sarahazouvi@Sarahs-MacBook-Air.local>
2023-08-17 16:27:25 +0100

    chapter 17

---
+++ gui/src/lib.rs
@@ -0,0 +1,38 @@
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // code to draw button (out of scope)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(4, 4);
+    }
+}
